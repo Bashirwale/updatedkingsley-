efee61228928766c99e6358271869509663dcb02
bashir wale <bashabdulbash@gmail.com>
2022-07-14 08:42:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+       document.getElementById("navLinks").style.width = "100%";
+     }
+     
+     /* Close */
+function closeNav() {
+       document.getElementById("navLinks").style.width = "0";
+     }
